d40c64ec24a3bba2ec596fd8ba5df8ef5d77baeb
sneoo <epicshakil@gmail.com>
2022-11-10 01:02:22 +0600

    some changes

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -112,7 +112,7 @@ const Signup = () => {
               />
             </div>
             <div className="form-control mt-6">
-              <button type="submit" className="btn btn-primary">
+              <button type="submit" className="btn">
                 Sign Up
               </button>
             </div>
